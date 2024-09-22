dc84e64eee952c27889f0752ac29c1d663d07ce6
mallailqadrillah <mallailqadrillah43@gmail.com>
2024-09-22 12:53:47 +0700

    feat: login authentication

--- app/auth.py
+++ app/auth.py
@@ -1,13 +1,32 @@
-from flask import Blueprint, redirect, url_for, session
+from flask import Blueprint, redirect, url_for, session, render_template, request, flash
 
 # Create a Blueprint for the authentication routes
 auth = Blueprint('auth', __name__)
 
 # * Route endpoint login
-@auth.route('/login/<role>')
-def login(role):
-    # Set the user role in the session
-    session['role'] = role
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    # Redirect to the products page
-    return redirect(url_for('main.products'))
+        # check username and password is correct and set as role session
+        if username == 'cashier' and password == 'cashier':
+            session['role'] = 'Cashier'
+        elif username == 'admingudang' and password == 'admingudang':
+            session['role'] = 'Admin Gudang'
+        elif username == 'superadmin' and password == 'superadmin':
+            session['role'] = 'Super Admin'
+        else:
+            return redirect(url_for('auth.login'))
+
+        return redirect(url_for('main.products'))
+
+    return render_template('login.html')
+
+# * Route endpoint logout
+@auth.route('/logout')
+def logout():
+    # remove role session
+    session.pop('role', None)
+    return redirect(url_for('main.index'))
